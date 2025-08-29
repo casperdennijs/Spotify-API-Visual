5b7cc7a80b11d25161d73774ffd8d1d4bcff73af
Casper <casperdennijs+github@proton.me>
2025-08-29 10:49:54 +0200

    Implement canvas download feature

--- public/html2canvas.js
+++ public/html2canvas.js
@@ -1,1 +1,27 @@
+document.getElementById("download").addEventListener("click", () => {
+    const element = document.getElementById("poster");
+    const title = document.querySelector("form#title input").value;
 
+    function formatDate(date) {
+        const d = date.getDate();
+        const m = date.getMonth() + 1;
+        const y = date.getFullYear();
+        const h = date.getHours();
+        const min = date.getMinutes();
+
+        return `${d}${m}${y}${h}${min.toString().padStart(2, '0')}`;
+    }
+    const now = new Date();
+
+    html2canvas(element, {
+        backgroundColor: null,
+        useCORS: true
+    }).then(canvas => {
+        const dataUrl = canvas.toDataURL("image/png");
+
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = `${title}_${formatDate(now)}`;
+        link.click();
+    });
+});
